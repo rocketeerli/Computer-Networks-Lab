fc9099ca025d536c5d198abd4bb28ce511abc464
rocketeerLi <31384630+rocketeerli@users.noreply.github.com>
2018-11-01 01:13:59 +0800

    版本-1  一堆错误

---
+++ lab1/main.cpp
@@ -0,0 +1,430 @@
+//#include "stdafx.h"
+#include <stdio.h>
+#include <Windows.h>
+#include <process.h>
+#include <string.h>
+
+// Codeblocks 使用的是 MingGW 来编译，MingGW不支持#pragma comment(lib,"Ws2_32.lib") 的写法
+// 该命令是静态链接 Ws2_32.lib 库， 可以在设置里，加上 -lws2_32 或 -lwsock32
+//#pragma comment(lib,"Ws2_32.lib")
+
+#define MAXSIZE 65507 //发送数据报文的最大长度
+#define HTTP_PORT 80 //http 服务器端口
+
+#define INVILID_WEBSITE "http://www.hit.edu.cn/"            //被屏蔽的网站
+
+#define FISHING_WEB_SRC "http://www.qq.com/"         //钓鱼的源网址
+#define FISHING_WEB_DEST "jwts.hit.edu.cn/"    //钓鱼的目的网址
+
+
+//Http 重要头部数据
+struct HttpHeader{
+    char method[4]; // POST 或者 GET，注意有些为 CONNECT，本实验暂 不考虑
+    char url[1024]; // 请求的 url
+    char host[1024]; // 目标主机
+    char cookie[1024 * 10]; //cookie
+    HttpHeader(){
+        ZeroMemory(this,sizeof(HttpHeader));
+    }
+};
+
+BOOL InitSocket();
+void ParseHttpHead(char *buffer,HttpHeader * httpHeader);
+BOOL ConnectToServer(SOCKET *serverSocket,char *host);
+unsigned int __stdcall ProxyThread(LPVOID lpParameter);
+
+void makeFileName(char *url, char *fileName);
+BOOL ParseDate(char *buffer, char *field, char *tempDate);
+void makeNewHTTP(char *buffer, char *value);
+void makeCache(char *buffer, char *url);
+void getCache(char *buffer, char *filename);
+
+//代理相关参数
+SOCKET ProxyServer;
+sockaddr_in ProxyServerAddr;
+const int ProxyPort = 10240;
+
+//缓存相关参数
+BOOL haveCache = false;
+BOOL needCache = false;
+
+//由于新的连接都使用新线程进行处理，对线程的频繁的创建和销毁特别浪费资源
+//可以使用线程池技术提高服务器效率
+//const int ProxyThreadMaxNum = 20;
+//HANDLE ProxyThreadHandle[ProxyThreadMaxNum] = {0};
+//DWORD ProxyThreadDW[ProxyThreadMaxNum] = {0};
+
+struct ProxyParam{
+    SOCKET clientSocket;
+    SOCKET serverSocket;
+};
+
+int main(int argc, char* argv[]) {
+    printf("代理服务器正在启动\n");
+    printf("初始化...\n");
+    if(!InitSocket()){
+        printf("socket 初始化失败\n");
+        return -1;
+    }
+    printf("代理服务器正在运行，监听端口 %d\n",ProxyPort);
+    SOCKET acceptSocket = INVALID_SOCKET;
+    ProxyParam *lpProxyParam;
+    HANDLE hThread;
+    DWORD dwThreadID;
+    //代理服务器不断监听
+
+    while(true){
+        acceptSocket = accept(ProxyServer,NULL,NULL);
+        lpProxyParam = new ProxyParam;
+        if(lpProxyParam == NULL){
+            continue;
+        }
+        lpProxyParam->clientSocket = acceptSocket;
+        hThread = (HANDLE)_beginthreadex(NULL, 0, &ProxyThread,(LPVOID)lpProxyParam, 0, 0);
+        CloseHandle(hThread);
+        Sleep(200);
+    }
+    closesocket(ProxyServer);
+    WSACleanup();
+    return 0;
+}
+
+//************************************
+// Method:    InitSocket
+// FullName:  InitSocket
+// Access:    public
+// Returns:   BOOL
+// Qualifier: 初始化套接字
+//************************************
+BOOL InitSocket(){
+    //加载套接字库（必须）
+    WORD wVersionRequested;
+    WSADATA wsaData;        //WSADATA结构体中主要包含了系统所支持的Winsock版本信息
+    //套接字加载时错误提示
+    int err;
+    //版本 2.2
+    wVersionRequested = MAKEWORD(2, 2);
+    //加载 dll 文件 Scoket 库
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if(err != 0){
+        //找不到 winsock.dll
+        printf("加载 winsock 失败，错误代码为: %d\n", WSAGetLastError());
+        return FALSE;
+    }
+    //LOBYTE()得到一个16bit数最低（最右边）那个字节
+    //HIBYTE()得到一个16bit数最高（最左边）那个字节
+    //判断打开的是否是2.2版本
+    if(LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) !=2)   {
+        printf("不能找到正确的 winsock 版本\n");
+        WSACleanup();
+        return FALSE;
+    }
+    //AF_INET,PF_INET	IPv4 Internet协议
+    //SOCK_STREAM	Tcp连接，提供序列化的、可靠的、双向连接的字节流。支持带外数据传输
+    ProxyServer = socket(AF_INET, SOCK_STREAM, 0);
+    if(INVALID_SOCKET == ProxyServer){
+        printf("创建套接字失败，错误代码为：%d\n",WSAGetLastError());
+        return FALSE;
+    }
+    ProxyServerAddr.sin_family = AF_INET;
+    ProxyServerAddr.sin_port = htons(ProxyPort);      //将整型变量从主机字节顺序转变成网络字节顺序
+    ProxyServerAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+    if(bind(ProxyServer,(SOCKADDR*)&ProxyServerAddr,sizeof(SOCKADDR)) == SOCKET_ERROR){
+        printf("绑定套接字失败\n");
+        return FALSE;
+    }
+    if(listen(ProxyServer, SOMAXCONN) == SOCKET_ERROR){
+        printf("监听端口%d 失败",ProxyPort);
+        return FALSE;
+    }
+    return TRUE;
+}
+//************************************
+// Method:    ProxyThread
+// FullName:  ProxyThread
+// Access:    public
+// Returns:   unsigned int __stdcall
+// Qualifier: 线程执行函数
+// Parameter: LPVOID lpParameter
+//************************************
+unsigned int __stdcall ProxyThread(LPVOID lpParameter){
+    char Buffer[MAXSIZE];
+    char *CacheBuffer, *DateBuffer;
+
+    //缓存使用到的变量
+    char fileName[100], fileBuffer[MAXSIZE] ,dateStr[50];
+    char *field = "Date";
+
+    HttpHeader* httpHeader = new HttpHeader();
+    ZeroMemory(Buffer,MAXSIZE);
+    SOCKADDR_IN clientAddr;
+    int length = sizeof(SOCKADDR_IN);
+    int recvSize;
+    int ret;
+    recvSize = recv(((ProxyParam *)lpParameter)->clientSocket,Buffer,MAXSIZE,0);
+    if(recvSize <= 0){
+        goto error;
+    }
+    CacheBuffer = new char[recvSize + 1];
+    ZeroMemory(CacheBuffer, recvSize + 1);
+    memcpy(CacheBuffer, Buffer, recvSize);
+
+    DateBuffer = new char[recvSize + 1];
+    ZeroMemory(DateBuffer, recvSize + 1);
+    memcpy(DateBuffer, Buffer, recvSize);
+
+    //解析http首部
+    ParseHttpHead(CacheBuffer, httpHeader);
+
+    //网站过滤：屏蔽一个网站
+    if (strcmp (httpHeader->url, INVILID_WEBSITE) == 0) {
+        printf("-------------Sorry!!!该网站已被屏蔽----------------\n");
+        goto error;
+    }
+    //用户过滤：支持/不支持某些用户访问外部网站
+    printf("Buffer为----------------------------------\n%s", Buffer);
+    //网站引导：将访问网址转到其他网站
+    if (strcmp(httpHeader->url, FISHING_WEB_SRC) == 0) {
+        printf("-------------不好意思，。、您已被钓鱼，嘿嘿----------------\n");
+        //错误的
+        memcpy(httpHeader->url, FISHING_WEB_DEST, strlen(FISHING_WEB_DEST));
+        memcpy(httpHeader->host, "jwts.hit.edu.cn", strlen("jwts.hit.edu.cn"));
+        printf("目的网址： %s",httpHeader->host);
+        printf("-------------已从源网址：%s 转到 目的网址 ：%s ----------------\n", FISHING_WEB_SRC,FISHING_WEB_DEST);
+    }
+
+    //缓存
+    ZeroMemory(fileName, 100);
+    ZeroMemory(dateStr, 50);
+    ZeroMemory(fileBuffer, MAXSIZE);
+    makeFileName(httpHeader->url, fileName);
+
+    //文件操作
+    FILE *fp;
+    if ((fp = fopen(fileName, "rb")) != NULL) {
+        printf("存在缓存....");
+        fread(fileBuffer, sizeof(char), MAXSIZE, fp);
+        fclose(fp);
+        ParseDate(fileBuffer, field, dateStr);
+        printf("dateStr: %s\n", dateStr);
+        makeNewHTTP(Buffer, dateStr);
+		printf("\n======改造后的请求报文======\n%s\n", Buffer);
+		haveCache = true;
+    }
+
+    delete CacheBuffer;
+    delete DateBuffer;
+    if(!ConnectToServer(&((ProxyParam *)lpParameter)->serverSocket,httpHeader->host)) {
+        goto error;
+    }
+    printf("代理连接主机 %s 成功\n",httpHeader->host);
+
+    //有缓存时，判断返回的状态码是否是304，若是则将缓存的内容发送给客户端
+	if (haveCache == true) {
+		getCache(Buffer, fileName);
+	}
+	//将目标服务器返回的数据直接转发给客户端
+	printf("\n======响应报文======\n%s\n", Buffer);
+	if (needCache == true) {
+		makeCache(Buffer, httpHeader->url);  //缓存报文
+	}
+
+    //将客户端发送的 HTTP 数据报文直接转发给目标服务器
+    ret = send(((ProxyParam *)lpParameter)->serverSocket,Buffer,strlen(Buffer) + 1,0);
+    //等待目标服务器返回数据
+    recvSize = recv(((ProxyParam *)lpParameter)->serverSocket,Buffer,MAXSIZE,0);
+    if(recvSize <= 0){
+        goto error;
+    }
+    //将目标服务器返回的数据直接转发给客户端
+    ret = send(((ProxyParam *)lpParameter)->clientSocket,Buffer,sizeof(Buffer),0);
+    //错误处理
+    error:
+    printf("关闭套接字\n");
+    Sleep(200);
+    closesocket(((ProxyParam*)lpParameter)->clientSocket);
+    closesocket(((ProxyParam*)lpParameter)->serverSocket);
+    delete  lpParameter;
+    _endthreadex(0);
+    return 0;
+}
+//************************************
+// Method:    ParseHttpHead
+// FullName:  ParseHttpHead
+// Access:    public
+// Returns:   void
+// Qualifier: 解析 TCP 报文中的 HTTP 头部
+// Parameter: char * buffer
+// Parameter: HttpHeader * httpHeader
+//************************************
+void ParseHttpHead(char *buffer,HttpHeader * httpHeader){
+    printf("Buffer为----------------------------------\n%s", buffer);
+    char *p;
+    const char * delim = "\r\n";
+    p = strtok(buffer,delim); // 第一次调用，第一个参数为被分解的字符串
+    //提取第一行
+    printf("%s\n",p);
+    if(p[0] == 'G'){
+        //GET 方式
+        memcpy(httpHeader->method,"GET",3);
+        memcpy(httpHeader->url,&p[4],strlen(p) -13); //'Get' 和 'HTTP/1.1' 各占 3 和 8 个，再加上俩空格，一共13个
+    }
+    else if(p[0] == 'P'){
+        //POST 方式
+        memcpy(httpHeader->method,"POST",4);
+        memcpy(httpHeader->url,&p[5],strlen(p) - 14); //'Post' 和 'HTTP/1.1' 各占 4 和 8 个，再加上俩空格，一共14个
+    }
+    printf("访问的url是 ： %s\n",httpHeader->url);
+    p = strtok(NULL,delim);              // 第二次调用，需要将第一个参数设为 NULL
+    while(p){
+        switch(p[0]){
+            case 'H'://Host
+                memcpy(httpHeader->host,&p[6],strlen(p) - 6);
+                break;
+            case 'C'://Cookie
+                if(strlen(p) > 8){
+                    char header[8];
+                    ZeroMemory(header,sizeof(header));
+                    memcpy(header,p,6);
+                    if(!strcmp(header,"Cookie")){
+                        memcpy(httpHeader->cookie,&p[8],strlen(p) -8);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        p = strtok(NULL,delim);
+    }
+}
+//************************************
+// Method:    ConnectToServer
+// FullName:  ConnectToServer
+// Access:    public
+// Returns:   BOOL
+// Qualifier: 根据主机创建目标服务器套接字，并连接
+// Parameter: SOCKET * serverSocket
+// Parameter: char * host
+//************************************
+BOOL ConnectToServer(SOCKET *serverSocket,char *host){
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(HTTP_PORT);
+    HOSTENT *hostent = gethostbyname(host);
+    if(!hostent){
+        return FALSE;
+    }
+    in_addr Inaddr = *( (in_addr*) *hostent->h_addr_list);
+    serverAddr.sin_addr.s_addr = inet_addr(inet_ntoa(Inaddr));
+    *serverSocket = socket(AF_INET,SOCK_STREAM,0);
+    if(*serverSocket == INVALID_SOCKET){
+        return FALSE;
+    }
+    if(connect(*serverSocket,(SOCKADDR *)&serverAddr,sizeof(serverAddr)) == SOCKET_ERROR){
+        closesocket(*serverSocket);
+        return FALSE;
+    }
+    return TRUE;
+}
+
+//根据url构造文件名
+void makeFileName(char *url, char *fileName) {
+	char *p = fileName;
+	while (*url != '\0') {
+		if (*url != '/' && *url != ':' && *url != '.') {
+			*p++ = *url;
+		}
+		url++;
+	}
+}
+//分析HTTP头部的field字段，如果包含该field则返回true，并获取日期
+BOOL ParseDate(char *buffer, char *field, char *tempDate) {
+	char *p, temp[5], *buffer2;
+	//const char *field = "If-Modified-Since";
+	const char *delim = "\r\n";
+	ZeroMemory(temp, 5);
+	ZeroMemory(buffer2, strlen(buffer) + 1);
+	memcpy(buffer2, buffer, strlen(buffer));
+	p = strtok(buffer2, delim);
+	//printf("%s\n", p);
+	int len = strlen(field) + 2;
+	while (p) {
+		if (strstr(p, field) != NULL) {
+			memcpy(tempDate, &p[len], strlen(p) - len);
+			//printf("tempDate: %s\n", tempDate);
+			return true;
+		}
+		p = strtok(NULL, delim);
+	}
+	return false;
+}
+
+//改造HTTP请求报文
+void makeNewHTTP(char *buffer, char *value) {
+	const char *field = "Host";
+	const char *newfield = "If-Modified-Since: ";
+	//const char *delim = "\r\n";
+	char temp[MAXSIZE];
+	ZeroMemory(temp, MAXSIZE);
+	char *pos = strstr(buffer, field);
+	for (int i = 0; i < strlen(pos); i++) {
+		temp[i] = pos[i];
+	}
+	*pos = '\0';
+	while (*newfield != '\0') {  //插入If-Modified-Since字段
+		*pos++ = *newfield++;
+	}
+	while (*value != '\0') {
+		*pos++ = *value++;
+	}
+	*pos++ = '\r';
+	*pos++ = '\n';
+	for (int i = 0; i < strlen(temp); i++) {
+		*pos++ = temp[i];
+	}
+	//printf("buffer: %s\n", buffer);
+}
+
+//进行缓存
+void makeCache(char *buffer, char *url) {
+	char *p, num[10], tempBuffer[MAXSIZE + 1];
+	const char * delim = "\r\n";
+	ZeroMemory(num, 10);
+	ZeroMemory(tempBuffer, MAXSIZE + 1);
+	memcpy(tempBuffer, buffer, strlen(buffer));
+	p = strtok(tempBuffer, delim);//提取第一行
+	memcpy(num, &p[9], 3);
+	if (strcmp(num, "200") == 0) {  //状态码是200时缓存
+		//printf("url : %s\n", url);
+		char filename[100] = { 0 };  // 构造文件名
+		makeFileName(url, filename);
+		//printf("filename : %s\n", filename);
+		FILE *out;
+		if ((out = fopen(filename, "wb")) == 0) {
+			fwrite(buffer, sizeof(char), strlen(buffer), out);
+			fclose(out);
+		}
+		printf("\n报文已缓存！\n");
+	}
+}
+
+//获取缓存
+void getCache(char *buffer, char *filename) {
+	char *p, num[10], tempBuffer[MAXSIZE + 1];
+	const char * delim = "\r\n";
+	ZeroMemory(num, 10);
+	ZeroMemory(tempBuffer, MAXSIZE + 1);
+	memcpy(tempBuffer, buffer, strlen(buffer));
+	p = strtok(tempBuffer, delim);//提取第一行
+	memcpy(num, &p[9], 3);
+	if (strcmp(num, "304") == 0) {  //主机返回的报文中的状态码为304时返回已缓存的内容
+		printf("获取本地缓存！\n");
+		ZeroMemory(buffer, strlen(buffer));
+		FILE *in;
+		if ((in = fopen(filename, "rb")) == 0) {
+			fread(buffer, sizeof(char), MAXSIZE, in);
+			fclose(in);
+		}
+		needCache = false;
+	}
+}
